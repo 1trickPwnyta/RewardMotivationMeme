a53e2da01514495f8c4f67f47640ae1fd567e9eb
1trickPwnyta <sarahpace37@gmail.com>
2024-03-21 16:50:26 -0700

    Update to 1.5

---
+++ 1.5/Source/Patch_Quest.cs
@@ -0,0 +1,83 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace RewardMotivationMeme
+{
+    [HarmonyPatch(typeof(Quest))]
+    [HarmonyPatch(nameof(Quest.End))]
+    public static class Patch_Quest_End
+    {
+        public static void Postfix(Quest __instance, QuestEndOutcome outcome)
+        {
+            Debug.Log(__instance.name);
+            Debug.Log(__instance.description);
+            Debug.Log(outcome.ToString());
+
+            // Exceptions
+            if (__instance.name == "RefugeePodCrash")
+            {
+                return;
+            }
+            if (__instance.name == "WandererJoins")
+            {
+                return;
+            }
+            if (__instance.name == "WandererJoinAbasia")
+            {
+                return;
+            }
+            if (__instance.name == "The Deserter")
+            {
+                return;
+            }
+            if (__instance.name.Contains(" Work Site "))
+            {
+                return;
+            }
+            if (__instance.name.Contains("Betrayal Offer"))
+            {
+                return;
+            }
+            if (__instance.description != null && __instance.description.ToString().Contains("long-range mineral scanner"))
+            {
+                return;
+            }
+            if (__instance.description != null && __instance.description.ToString().Contains(" begging "))
+            {
+                return;
+            }
+            if (__instance.description != null && __instance.description.ToString().Contains("desperate refugee"))
+            {
+                return;
+            }
+            if (__instance.description != null && __instance.description.ToString().Contains("sanguophage master"))
+            {
+                return;
+            }
+
+            // Create the thought based on the quest outcome
+            ThoughtDef thoughtDef;
+            if (outcome != QuestEndOutcome.Fail)
+            {
+                thoughtDef = ThoughtDef.Named("RewardMotivationMeme_QuestCompleted");
+            }
+            else
+            {
+                thoughtDef = ThoughtDef.Named("RewardMotivationMeme_QuestFailed");
+            }
+
+            // Give the thought to every colonist with the precept
+            PreceptDef preceptDef = DefDatabase<PreceptDef>.GetNamed("RewardMotivationMeme_QuestsMandatory");
+            foreach (Pawn pawn in PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonists)
+            {
+                if (pawn.ideo.Ideo.HasPrecept(preceptDef) && pawn.needs.mood != null)
+                {
+                    Precept precept = pawn.ideo.Ideo.GetPrecept(preceptDef);
+                    pawn.needs.mood.thoughts.memories.TryGainMemory(thoughtDef, null, precept);
+                }
+            }
+        }
+    }
+}
---
+++ 1.5/Source/RewardMotivationMemeMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace RewardMotivationMeme
+{
+    public class RewardMotivationMemeMod : Mod
+    {
+        public const string PACKAGE_ID = "rewardmotivationmeme.1trickpwnyta";
+        public const string PACKAGE_NAME = "Reward Motivation Meme";
+
+        public RewardMotivationMemeMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
